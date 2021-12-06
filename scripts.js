8fd4ace7580be754e6c38938afa304f476d5b6f9
Kayla Gervais <61392996+kgervais@users.noreply.github.com>
2021-12-06 18:02:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,47 +1,13 @@
-const stopRace = "stop.png";
-const startRace = "go.png";
 
-var finishLine = (window.innerWidth - (window.innerWidth * 0.20));
-var declareWinner = false;
-
-function ChangeLightColor() {
-    var raceLight = document.getElementById("raceLight");
-    var raceLightSrc = raceLight.getAttribute("src");
-    if (raceLightSrc === startRace) {
-        raceLight.src = stopRace;
-        StartRace();
-    } else if (raceLightSrc === stopRace) {
-        raceLight.src = startRace;
-        ResetRace();
-    }
-}
-
-/*Race Starts*/
-function StartRace() {
-    declareWinner = false;
-    ZoomZoom("nyan");
-    ZoomZoom("pusheen");
-}
-
-function ZoomZoom(racerId) {
-    var racer = document.getElementById(racerId);
-    var position = parseInt(racer.style.left, 10);
-    if (isNaN(position)) {
-        position = 0;
-    }
-
-    if (position < finishLine) {
-        position += randomNumber(1, 10);
-        racer.style.left = position + "px";
-        setTimeout('ZoomZoom("'+ racerId +'")', randomNumber(1, 10));
-    }
-     else {
-        if (position >= finishLine && !declareWinner) {
+            else {
+              pusheenWin.style.visibility="visible";
+            }
             declareWinner = true;
             var winnerImg = document.getElementById(racerId);
             winnerImg.addEventListener("click", ChangeLightColor);
         }
-    }}
+    }
+  }
 
     /*    Reset race */
     function ResetRace() {
